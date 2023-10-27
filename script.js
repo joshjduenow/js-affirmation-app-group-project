9ac15fc3e67a4e7e748ff78bcc37d8bd2a2a4aad
Joshua Duenow <joshjduenow@gmail.com>
2023-10-27 10:21:54 -0500

    added emojis

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 onReady();
 
+
+
+function fireEmoji() {
+  console.log("Fire emoji works!!");
+  
+  let emojis = document.getElementById('emojis');
+
+  emojis.innerHTML += `<p>🔥</p>`
+}
+
+function iceEmoji() {
+  console.log("Ice emoji works!!");
+  
+  let emojis = document.getElementById('emojis');
+
+  emojis.innerHTML += `<p>❄️</p>`
+  
+}
 function onReady() {
   console.log('Javascript is working!');
 }
