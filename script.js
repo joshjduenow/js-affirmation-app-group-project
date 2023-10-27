b038374a5fdb13406c269399a12f6c704d8fb683
Joshua Duenow <joshjduenow@gmail.com>
2023-10-27 11:26:48 -0500

    completed step 3

--- script.js
+++ script.js
@@ -16,8 +16,34 @@ function iceEmoji() {
   let emojis = document.getElementById('emojis');
 
   emojis.innerHTML += `<p>❄️</p>`
+
+}
+function handleSubmit(event) {
+  console.log(event);
+
+  event.preventDefault();
+  let affirmation = document.getElementById('affirmationText');
+  let author = document.getElementById('authorText');
+
+  console.log(author, affirmation);
+
+  let tables = document.getElementById('tableBody');
+  tables.innerHTML += 
+  `<tr>
+  <td>${affirmation.value}</td>
+  <td>${author.value}</td>
+  <td><button onclick="deleteText(event)">❌</button></td>
+   </tr>`
+   affirmation.value = ""
+   author.value = ""
   
 }
+
+function deleteText(event) {
+  event.target.parentElement.parentElement.remove();
+  
+}
+
 function onReady() {
   console.log('Javascript is working!');
 }
